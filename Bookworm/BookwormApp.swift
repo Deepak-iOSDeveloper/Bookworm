fff43f3cbd2a70862fc5d6b4a4df1b152f506fff
deepak <deepakjgrt99@gmail.com>
2025-06-20 10:24:43 +0530

    Initial Commit

---
+++ Bookworm/BookwormApp.swift
@@ -0,0 +1,17 @@
+//
+//  BookwormApp.swift
+//  Bookworm
+//
+//  Created by DEEPAK BEHERA on 20/06/25.
+//
+
+import SwiftUI
+
+@main
+struct BookwormApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
